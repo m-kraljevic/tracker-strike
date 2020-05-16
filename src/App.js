bfbe607bba55fd4cabe12b5d963afce61be1b3d2
Matthew Kraljevic <mattkraljevic@gmail.com>
2020-05-16 12:12:01 -0500

    added in react-apexcharts and a graph with dummy data

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
+import Chart from 'react-apexcharts';
 
 class App extends Component {
   state = {
-      kdRatio: 0.5,
-      gamesWon: 100,
-      timePracticed: 90,
-      newPracticeValue: '',
-      newGameRoundsWon: '',
-      newGameRoundsLost: '',
-      newGameRoundsK: '',
-      newGameRoundD: ''
+    kdRatio: 0.5,
+    gamesWon: 100,
+    timePracticed: 90,
+    newPracticeValue: '',
+    newGameRoundsWon: '',
+    newGameRoundsLost: '',
+    newGameRoundsK: '',
+    newGameRoundD: '',
+    options: {
+      xaxis: {
+        categories: ['a', 'b', 'c', 'd', 'e']
+      }
+    },
+    series: [
+      {
+        name: 'test',
+        data: [1, 2, 3, 4, 5]
+      }
+    ]
   }
-  
+
   render() {
     return (
       <div>
+        <div>Overall stats</div>
+        <div>k/d ratio, games won, time practiced</div>
+        <div>
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="bar"
+            height="450"
+            width="50%" />
+        </div>
+        <div>Add practice time
           <div>
-              Overall stats
+            <input type='text'></input>
+            <button>Submit</button>
+          </div>
+        </div>
+        <div>
+          Add game results
+              <div>
+            Rounds won:<input type='text'></input>
           </div>
           <div>
-              k/d ratio, games won, time practiced
+            Rounds lost:<input type='text'></input>
           </div>
           <div>
-              graph of k/d ratio to games won
+            K:<input type='text'></input>
           </div>
           <div>
-              Add practice time
-              <div>
-                  <input type='text'></input>
-                  <button>Submit</button>
-              </div>
+            D:<input type='text'></input>
           </div>
-          <div>
-              Add game results
-              <div>
-                  Rounds won:<input type='text'></input>
-              </div>
-              <div>
-                  Rounds lost:<input type='text'></input>
-              </div>
-              <div>
-                  K:<input type='text'></input>
-              </div>
-              <div>
-                  D:<input type='text'></input>
-              </div>
-              <button>Submit</button>
-          </div>
+          <button>Submit</button>
+        </div>
       </div>
-      )
+    )
   }
 }
 
