20dc702610de9be66b397510e9fef150340dec8d
m-kraljevic <57931772+m-kraljevic@users.noreply.github.com>
2020-05-15 23:47:34 -0500

    React basic changes (#3)
    * added in react basic code
    
    * changed folder structure
    
    * changed to class based component with a state to represent dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import './App.css';
+import Frontend from './Frontend'
 
 function App() {
   return (
-    <div>
-      <div>
-        Overall stats
-        <div>
-          k/d ratio, games won, time practiced
-        </div>
-        <div>
-          graph of k/d ratio to games won
-        </div>
-      </div>
-      <div>
-        Add practice time
-      </div>
-      <div>
-        Add game results
-      </div>
-    </div>
+    <Frontend />
   );
 }
 
