b1c8beb4d96d81b2801aac0b2bd19d989b2759c1
Matthew Kraljevic <mattkraljevic@gmail.com>
2020-05-15 22:53:25 -0500

    added in react basic code

---
+++ tracker-strike/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        Overall stats
+        <div>
+          k/d ratio, games won, time practiced
+        </div>
+        <div>
+          graph of k/d ratio to games won
+        </div>
+      </div>
+      <div>
+        Add practice time
+      </div>
+      <div>
+        Add game results
+      </div>
+    </div>
+  );
+}
+
+export default App;
