43af5277891e769d77c396175332f0c92ea00ae6
m-kraljevic <57931772+m-kraljevic@users.noreply.github.com>
2020-05-27 20:11:56 -0500

    changes to app.js, added new components and files for firebase (#7)
    * changes to app.js, added new components and files for firebase
    
    * added firebase connection, reading writing, changed graph library to fusioncharts
    
    * removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,68 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
-import Chart from 'react-apexcharts';
+import firebase from './services/firebase';
+import Login from './Login';
+import Home from './Home'
 
 class App extends Component {
-  state = {
-    kdRatio: 0.5,
-    gamesWon: 100,
-    timePracticed: 90,
-    newPracticeValue: '',
-    newGameRoundsWon: '',
-    newGameRoundsLost: '',
-    newGameRoundsK: '',
-    newGameRoundD: '',
-    options: {
-      xaxis: {
-        categories: ['a', 'b', 'c', 'd', 'e']
+
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+  
+
+
+  componentDidMount() {
+    this.authListener();
+    
+  }
+
+  updateK = (e) => {
+    this.setState({ k: e.target.value })
+  }
+
+  updateD = (e) => {
+    this.setState({ d: e.target.value })
+  }
+
+  updateFirebase = () => {
+    firebase.database().ref().set({
+      practice: 30,
+      k: this.state.k,
+      d: this.state.d
+    });
+  }
+
+  authListener() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('logged in');
+        this.setState({ user });
+        // console.log(user.uid);
       }
-    },
-    series: [
-      {
-        name: 'test',
-        data: [1, 2, 3, 4, 5]
+      else {
+        console.log('could not log in');
+        this.setState({ user: null })
       }
-    ]
+    })
   }
 
   render() {
+
     return (
-      <div>
-        <div>Overall stats</div>
-        <div>k/d ratio, games won, time practiced</div>
-        <div>
-          <Chart
-            options={this.state.options}
-            series={this.state.series}
-            type="bar"
-            height="450"
-            width="50%" />
-        </div>
-        <div>Add practice time
-          <div>
-            <input type='text'></input>
-            <button>Submit</button>
-          </div>
-        </div>
-        <div>
-          Add game results
-              <div>
-            Rounds won:<input type='text'></input>
-          </div>
-          <div>
-            Rounds lost:<input type='text'></input>
-          </div>
-          <div>
-            K:<input type='text'></input>
-          </div>
-          <div>
-            D:<input type='text'></input>
-          </div>
-          <button>Submit</button>
-        </div>
+      <div className="App">
+        {this.state.user ? (<Home />) : (<Login />)}
+
       </div>
     )
   }
 }
 
 export default App;
+
