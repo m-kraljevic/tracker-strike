7f152f48797291f444afbf730c9ab8d864521ac3
Matthew Kraljevic <mattkraljevic@gmail.com>
2020-05-27 20:44:15 -0500

    travis CI build failed, using local build folder

---
+++ build/precache-manifest.221de255f680f7a64a4cd40757c27b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72df031a7120b1306d035890a1440fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0769ce570bb3fd50a00c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "184c1e367bbee8b960b7",
+    "url": "/static/js/2.d734f4b3.chunk.js"
+  },
+  {
+    "revision": "7637062a3e090034f93f0b870152f1a8",
+    "url": "/static/js/2.d734f4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0769ce570bb3fd50a00c",
+    "url": "/static/js/main.e29f1905.chunk.js"
+  },
+  {
+    "revision": "60b226ab2989516d3350",
+    "url": "/static/js/runtime-main.30b667e4.js"
+  }
+]);
