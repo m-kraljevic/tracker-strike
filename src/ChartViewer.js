8c5bc1ca81b92b477b5ab3c50b98ac9f0daa5d10
Matthew Kraljevic <mattkraljevic@gmail.com>
2020-05-28 20:12:23 -0500

    now chartviewer is passed entire career stats, aggregation is done in chartviewer so prevprops determines if there has been a change in length (change wouldn't) be detected if multiple stats were added on one dat

--- src/ChartViewer.js
+++ src/ChartViewer.js
@@ -30,7 +30,7 @@ const dataSource = {
             },
             title: "KD ratio"
         },
-        
+
         {
             plot: {
                 value: "Practice",
@@ -58,9 +58,39 @@ class ChartViewer extends React.Component {
 
     componentDidUpdate(prevProps) {
         console.log('Component updated');
-        if(this.props.values && prevProps.values && this.props.values.length !== prevProps.values.length) {
+        if (this.props.values && prevProps.values && this.props.values.length !== prevProps.values.length) {
             // console.log('Component updated')
-            this.onFetchData(this.props.values);
+
+            let KDRatio = null;
+            let graphValues = [];
+            let graphValuesByDay = {};
+            console.log(JSON.stringify(this.props.values));
+            Object.values(this.props.values).forEach(item => {
+                if (graphValuesByDay[item.date]) {
+                    graphValuesByDay[item.date].kills += item.kills;
+                    graphValuesByDay[item.date].deaths += item.deaths;
+                    graphValuesByDay[item.date].practice += item.practice;
+                }
+                else {
+                    graphValuesByDay[item.date] = { kills: item.kills, deaths: item.deaths, practice: item.practice };
+                }
+            });
+
+            Object.keys(graphValuesByDay).forEach(date => {
+                const item = graphValuesByDay[date];
+                if (item.deaths === 0) {
+                    KDRatio = item.kills;
+                }
+                else {
+                    KDRatio = item.kills / item.deaths;
+                }
+                graphValues.push([date, KDRatio, item.practice]);
+            })
+
+
+
+
+            this.onFetchData(graphValues);
         }
     }
 
@@ -69,7 +99,7 @@ class ChartViewer extends React.Component {
     }
 
     onFetchData(values) {
-        
+
         Promise.all([values, schemaFetch]).then(res => {
             const data = res[0];
             const schema = res[1];
--- src/Home.js
+++ src/Home.js
@@ -79,39 +79,14 @@ class Home extends Component {
 
     componentDidMount() {
         
-        let KDRatio = null;
         firebase.database().ref(firebase.auth().currentUser.uid).on('value', (snap) => {
             console.log('theres new values')
-            let graphValues = [];
-            let graphValuesByDay = {};
-            console.log(JSON.stringify(snap.val()));
-            Object.values(snap.val()).forEach(item => {
-                if(graphValuesByDay[item.date]) {
-                    graphValuesByDay[item.date].kills += item.kills;
-                    graphValuesByDay[item.date].deaths += item.deaths;
-                    graphValuesByDay[item.date].practice += item.practice;
-                }
-                else {
-                    graphValuesByDay[item.date] = {kills: item.kills, deaths: item.deaths, practice: item.practice};
-                }
-            });
-
-            Object.keys(graphValuesByDay).forEach(date => {
-                const item = graphValuesByDay[date];
-                if(item.deaths === 0) {
-                    KDRatio = item.kills;
-                }
-                else {
-                    KDRatio = item.kills/item.deaths;
-                }
-                graphValues.push([date, KDRatio, item.practice]);
-            })
-
-
-            console.log(graphValues);
-            this.setState({ careerStats: snap.val(), graphArray: graphValues });
+            
+            this.setState({ careerStats: snap.val() });
         });
         // this.setState({uid: firebase.auth().currentUser.uid});
+
+        
     }
 
     logout() {
@@ -146,7 +121,7 @@ class Home extends Component {
                         Total career deaths: {totalDeaths}
                     </div>
                 </div>
-                <ChartViewer values = {this.state.graphArray}/>
+                <ChartViewer values = {this.state.careerStats}/>
                 <div>Enter time practiced in minutes:
                 <input type="text" pattern="[0-9]*" onChange={this.handleChangePractice} value={this.state.practiceTime} />
                     <button disabled={this.state.practiceTime === ''} onClick={this.submitPracticeStats}>Submit practice time</button>
